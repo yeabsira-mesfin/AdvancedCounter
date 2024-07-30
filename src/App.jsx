3358193fbd44039b8e9b5d1bd3b3d78abaa323ac
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-07-30 06:42:32 -0400

    Cofigure Counter

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,21 @@ import { useState } from 'react';
 import Counter from './components/Counter/Counter.jsx';
 import Header from './components/Header.jsx';
 import { log } from './log.js';
+import ConfigureCounter from './components/ConfigureCounter.jsx';
 
 function App() {
   log('<App /> rendered');
-
-  const [enteredNumber, setEnteredNumber] = useState(0);
   const [chosenCount, setChosenCount] = useState(0);
-
-  function handleChange(event) {
-    setEnteredNumber(+event.target.value);
-  }
-
-  function handleSetClick() {
-    setChosenCount(enteredNumber);
-    setEnteredNumber(0);
-  }
+  
+   function handleSetCount(newCount){
+    setChosenCount(newCount)
+   }
 
   return (
     <>
       <Header />
       <main>
-       
+        <ConfigureCounter onSet={handleSetCount}/>
         <Counter initialCount={chosenCount} />
       </main>
     </>
--- src/components/ConfigureCounter.jsx
+++ src/components/ConfigureCounter.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React,{useState} from "react";
 
-export default function ConfigureCounter() {
+export default function ConfigureCounter({onSet}) {
+    const [enteredNumber, setEnteredNumber] = useState(0);
+   
+    function handleChange(event) {
+      setEnteredNumber(+event.target.value);
+    }
+  
+    function handleSetClick() {
+     onSet(enteredNumber)
+      setEnteredNumber(0);
+    }
   return (
     <section id="configure-counter">
       <h2>Set Counter</h2>
