5903840fa748a39aa2f66ebb993e4a987359b769
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-08-01 06:46:37 -0400

    useMemo

--- src/components/Counter/CounterHistory.jsx
+++ src/components/Counter/CounterHistory.jsx
@@ -24,7 +24,7 @@ export default function CounterHistory({ history }) {
   return (
     <ol>
       {history.map((count, index) => (
-        <HistoryItem key={count.id} count={count.value} />
+        <HistoryItem key={index} count={count.value} />
       ))}
     </ol>
   );
